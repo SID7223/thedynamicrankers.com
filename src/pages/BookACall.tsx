5a15527af7ac1db8289131b26c0eacd4d580c1bd
SID7223 <eric@thedynamicrankers.com>
2025-07-28 10:18:29 +0500

    Update BookACall.tsx

--- src/pages/BookACall.tsx
+++ src/pages/BookACall.tsx
@@ -57,7 +57,7 @@ const BookACall: React.FC = () => {
               name="book-a-call" 
               method="POST"
               data-netlify="true"
-              action="/thank-you"
+              action="/thank-you.html"
               className="space-y-6"
             >
               <input type="hidden" name="form-name" value="book-a-call" />
