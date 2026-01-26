2fc476587f220f98146fab8a7f0ac9e01406bdb8
SID7223 <eric@thedynamicrankers.com>
2026-01-26 15:59:23 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,6 @@ const MainPage = () => {
 
   // Determine which components to show based on route
   const isHomePage = location.pathname === '/';
-  const isContactPage = location.pathname === '/contact';
   
   return (
     <>
@@ -147,9 +146,7 @@ const MainPage = () => {
       {/* Show Hero only on homepage */}
       {isHomePage && <Hero />}
       
-      {/* Show Testimonials Landing on homepage */}      {/* Only show BusinessGrowth and Contact on homepage */}
-      {isHomePage && <BusinessGrowth />}
-      {isHomePage && <Contact />}
+      {/* Show Testimonials Landing on homepage */}
       {isHomePage && <TestimonialsLanding />}
       
       {/* Only show BusinessGrowth and Contact on homepage */}
@@ -195,7 +192,6 @@ function App() {
         <Suspense fallback={<LoadingSpinner />}>
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/contact" element={<MainPage />} />
             
             {/* Service Pages */}
             <Route path="/our-services" element={<OurServicesPage />} />
